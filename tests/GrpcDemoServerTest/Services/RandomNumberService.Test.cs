8206ef86ef379317d5ba01295936d11b3c22f4da
MeltpoolMartin <martinwachter@gmx.net>
2021-08-14 12:29:00 +0000

    Add further tests for checking input validation

--- tests/GrpcDemoServerTest/Services/RandomNumberService.Test.cs
+++ tests/GrpcDemoServerTest/Services/RandomNumberService.Test.cs
@@ -8,23 +8,63 @@ namespace GrpcDemo.Tests
 {
     public class RandomNumberServiceTests
     {
-        [Fact]
-        public void GetRandomInteger_ValidRange_ReturnsRandomInteger()
+        private readonly RandomNumberService _service;
+        private Mock<ServerCallContext> _context;
+
+        public RandomNumberServiceTests()
+        {
+            var logger = new Mock<ILogger<RandomNumberService>>();
+            _service = new RandomNumberService(logger.Object);
+            _context = new Mock<ServerCallContext>();
+        }
+
+        [Theory]
+        [InlineData(1, 5)]
+        [InlineData(2, 2)]
+        public void GetRandomInteger_ValidRange_ReturnsRandomInteger(int lowerLimit, int upperLimit)
         {
             // Arrange
-            var logger = new Mock<ILogger<RandomNumberService>>();
-            var service = new RandomNumberService(logger.Object);
-            var lowerLimit = 1;
-            var upperLimit = 5;
             var request = new RandomIntegerRequest { LowerLimit = lowerLimit, UpperLimit = upperLimit };
-            var context = new Mock<ServerCallContext>();
 
             // Act
-            var reply = service.GetRandomInteger(request: request, context: context.Object);
+            var reply = _service.GetRandomInteger(request, _context.Object);
 
             // Assert
             var randomInteger = reply.Result.RandomInteger;
             Assert.InRange<Int32>(randomInteger, lowerLimit, upperLimit);
+        }
+
+        [Theory]
+        [InlineData(5, 1)]
+        [InlineData(-1, -5)]
+        public void GetRandomInteger_LowerLimitIsGreaterThanUpperLimit_ReturnsMinusOne(int lowerLimit, int upperLimit)
+        {
+            // Arrange
+            var request = new RandomIntegerRequest { LowerLimit = lowerLimit, UpperLimit = upperLimit };
+
+            // Act
+            var reply = _service.GetRandomInteger(request, _context.Object);
+
+            // Assert
+            var randomInteger = reply.Result.RandomInteger;
+            Assert.Equal(-1, randomInteger);
+        }
+
+        [Theory]
+        [InlineData(-1, 5)]
+        [InlineData(1, -5)]
+        [InlineData(-5, -1)]
+        public void GetRandomInteger_NegativeLimits_ReturnsMinusOne(int lowerLimit, int upperLimit)
+        {
+            // Arrange
+            var request = new RandomIntegerRequest { LowerLimit = lowerLimit, UpperLimit = upperLimit };
+
+            // Act
+            var reply = _service.GetRandomInteger(request, _context.Object);
+
+            // Assert
+            var randomInteger = reply.Result.RandomInteger;
+            Assert.Equal(-1, randomInteger);
         }
     }
 }
